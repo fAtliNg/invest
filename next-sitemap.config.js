904a9e6106e9745e249ee629bcf2dd031ebe0a50
sergey <sergey@example.com>
2026-03-01 20:55:16 +0300

    новости

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@ const isDev = process.env.NEXT_PUBLIC_IS_DEV === 'true';
 
 module.exports = {
   siteUrl: isDev ? 'https://profit-case-dev.ru' : 'https://profit-case.ru',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   exclude: ['/server-sitemap.xml', '/404', '/customers', '/equities', '/robots.txt'],
   robotsTxtOptions: {
     policies: isDev
